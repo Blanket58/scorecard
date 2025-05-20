218d28779aaed95702cc611e8e0bd6a0994bb4f8
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2025-05-20 10:28:28 +0800

    Update __init__.py

--- scorecard/__init__.py
+++ scorecard/__init__.py
@@ -1,4 +1,4 @@
 from .iv import iv_table
 from .perf import gains_table, perf_eva, perf_psi
-from .card import prob2score, scorecard
+from .card import prob2score, scorecard, to_sql
 from .woebin import woebin, woebin_plot, woebin_ply
