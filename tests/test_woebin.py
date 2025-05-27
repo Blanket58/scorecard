c7893c3469f36fc9f8eec51c9c1465eed544bdbc
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2025-05-27 15:52:55 +0800

    Update test_woebin.py

--- tests/test_woebin.py
+++ tests/test_woebin.py
@@ -23,3 +23,21 @@ class TestWoebin:
         y = pd.Series([0, 1, 1, 1], name='y')
         boundary = DecisionTree._fit_numeric(X, y)
         assert boundary == [-np.inf, np.float64(1.5), np.inf]
+
+    def test_fit_enum_with_missing(self):
+        X = pd.Series([1, np.nan], name='x')
+        y = pd.Series([0, 1], name='y')
+        boundary = DecisionTree._fit_enum(X, y)
+        assert np.allclose(boundary, [(1,), (np.nan,)], equal_nan=True)
+
+    def test_fit_category_with_missing(self):
+        X = pd.Series(['first', 'second', 'third', np.nan], name='x')
+        y = pd.Series([0, 1, 1, 1], name='y')
+        boundary = DecisionTree._fit_category(X, y)
+        assert md5(str(boundary).encode()).hexdigest() == md5("[('first',), (nan, 'second', 'third')]".encode()).hexdigest()
+
+    def test_fit_numeric_with_missing(self):
+        X = pd.Series([1, 2, 3, np.nan], name='x')
+        y = pd.Series([0, 1, 1, 0], name='y')
+        boundary = DecisionTree._fit_numeric(X, y)
+        assert boundary == [-np.inf, 1.5, np.inf]
