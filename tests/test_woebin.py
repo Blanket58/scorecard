1825ebb08c65afba0df8379c00f66640187ba97c
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2025-09-30 16:18:19 +0800

    Update test_woebin.py

--- tests/test_woebin.py
+++ tests/test_woebin.py
@@ -3,7 +3,7 @@ from hashlib import md5
 import numpy as np
 import pandas as pd
 
-from scorecard.woebin import DecisionTree
+from scorecard.woebin import DecisionTree, ChiMerge
 
 
 class TestWoebin:
@@ -13,18 +13,24 @@ class TestWoebin:
         y = pd.Series([0, 1], name='y')
         boundary = DecisionTree._fit_enum(X, y)
         assert boundary == [(1,), (2,)]
+        boundary = ChiMerge._fit_enum(X, y)
+        assert boundary == [(1,), (2,)]
 
     def test_fit_category(self):
         X = pd.Series(['first', 'second', 'third', 'forth'], name='x')
         y = pd.Series([0, 1, 1, 1], name='y')
         boundary = DecisionTree._fit_category(X, y)
         assert boundary == [('first',), ('forth', 'second', 'third')]
+        boundary = ChiMerge._fit_category(X, y, bins_num=2)
+        assert boundary == [('first',), ('forth', 'second', 'third')]
 
     def test_fit_numeric(self):
         X = pd.Series([1, 2, 3, 4], name='x')
         y = pd.Series([0, 1, 1, 1], name='y')
         boundary = DecisionTree._fit_numeric(X, y)
-        assert boundary == [-np.inf, np.float64(1.5), np.inf]
+        assert boundary == [-np.inf, 1.5, np.inf]
+        boundary = ChiMerge._fit_numeric(X, y, bins_num=2)
+        assert boundary == [-np.inf, 1.06, np.inf]
 
     def test_fit_enum_with_missing(self):
         X = pd.Series([1, np.nan], name='x')
