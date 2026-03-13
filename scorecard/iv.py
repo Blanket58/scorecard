060c64003d507fdef568847a7f819fc38087afa7
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2026-03-13 14:55:12 +0800

    Refactor iv_table function for better readability

--- scorecard/iv.py
+++ scorecard/iv.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 def iv_table(bins):
     dt = pd.concat(bins, axis=0)
-    return dt[['variable', 'total_iv']].drop_duplicates().sort_values(by='total_iv', ascending=False, ignore_index=True)
+    return (
+        dt[["variable", "total_iv"]]
+        .drop_duplicates()
+        .sort_values(by="total_iv", ascending=False, ignore_index=True)
+    )
