56e4f639958d1ba0467c4d754f396c3de0809540
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2026-03-13 10:12:19 +0800

    Replace woebin imports with new encoders

--- scorecard/__init__.py
+++ scorecard/__init__.py
@@ -32,4 +32,4 @@ Main functions:
 from .card import card2sql, prob2score, scorecard
 from .iv import iv_table
 from .perf import gains_table, perf_eva, perf_psi
-from .woebin import woebin, woebin_plot, woebin_ply
+from .woebin import DecisionTreeWoeEncoder, ChiMergeWoeEncoder
